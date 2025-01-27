7224e8d17abaeb13f23a85e36c65ad123ec5ace4
Andrew <ajjagecko@gmail.com>
2025-01-27 11:13:59 -0500

    Lab 1 Itterative complete

--- labAssignment1/labAssignment1.m
+++ labAssignment1/labAssignment1.m
@@ -3,27 +3,34 @@ b = 60;
 f = 6;
 ps = 0.006;
 xNumPix = 752;
+yNumPix = 480;
 cxLeft = xNumPix/2;
 cxRight = xNumPix/2;
 
-P = [0,0,10]';
-
-camLeft = CentralCamera('focal', 0.006, 'pixel', 0.000006, 'resolution', [752, 480], 'centre', [752/2, 480/2]);
+camLeft = CentralCamera('focal', f/1000, 'pixel', ps/1000, 'resolution', [xNumPix, yNumPix], 'centre', [xNumPix/2, yNumPix/2], 'name', 'Left');
 Tcam = SE3(-0.03,0,0);
 camLeft.T = Tcam;
-left = camLeft.project(P);
-camLeft.plot(P);
 
-camRight = CentralCamera('focal', 0.006, 'pixel', 0.000006, 'resolution', [752, 480], 'centre', [752/2, 480/2]);
+camRight = CentralCamera('focal', f/1000, 'pixel', ps/1000, 'resolution', [xNumPix, yNumPix], 'centre', [xNumPix/2, yNumPix/2], 'name', 'Right');
 Tcam = SE3(0.03,0,0);
 camRight.T = Tcam;
-right = camRight.project(P);
-camRight.plot(P);
 
-xLeft = left(1);
-xRight = right(1);
+depth = 1:0.1:10;
+numPoints = length(depth);
+xLeft = zeros(1,numPoints);
+xRight = zeros(1,numPoints);
+calcDepth = zeros(1,numPoints);
 
-d = (abs((xLeft - cxLeft)-(xRight-cxRight))*ps); %disparity [mm]
-Z = (b *f)/d; %depth [mm]
+for counter = 1:numPoints
+    P = [0,0,depth(counter)]';
+    leftPixel = camLeft.project(P);
+    rightPixel = camRight.project(P);
+    xLeft(counter) = leftPixel(1);
+    xRight(counter) = rightPixel(1);
+    d = (abs((xLeft(counter) - cxLeft)-(xRight(counter)-cxRight))*ps); %disparity [mm]
+    Z = (b*f)/d; %depth [mm]
+    calcDepth(counter) = Z/1000;
+end
 
-
+error = depth - calcDepth;
+plot(depth,error)
